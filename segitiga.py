94b2d6c8cec997d1954172dcf9fff195f806bf65
tio2401010006 <tyoade74@gmail.com>
2024-12-01 11:56:25 +0700

    commit pertama

---
+++ segitiga.py
@@ -0,0 +1,18 @@
+#JUDUL CONTENT
+print ("Hitung Luas Segitiga")
+
+#INPUT
+a = input("alas : ")
+t = input("tinggi : ")
+
+#CETAK 
+print("alas : ",a)
+print("tinggi : ",t)
+
+#PROSES HITUNG
+
+L = 0.5*float(a)*float(t)
+
+#OUTPUT/ MENAMPILKAN HASIL
+print("Ini adalah luas segitiga")
+print(L)
